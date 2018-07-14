e4a1c5a68b3684c484244a647fab446453ada3d7
KentoMoriwaki <kento@wantedly.com>
2018-07-14 10:58:11 +0900

    Add active model serializer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
+  include ActionController::Serialization
 
   def show
     user = User.find(params[:id])
-    render json: user
+    render json: user, fields: []
   end
 end
