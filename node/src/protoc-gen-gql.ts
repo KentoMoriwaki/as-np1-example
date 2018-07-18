736befdac91323eed7576e33caac9b4c4632af97
KentoMoriwaki <kento@wantedly.com>
2018-07-18 10:42:44 +0900

    Generate .graphql

--- node/src/protoc-gen-gql.ts
+++ node/src/protoc-gen-gql.ts
@@ -1,7 +1,10 @@
 import streamToPromise from "stream-to-promise";
 import pluginPb from "google-protobuf/google/protobuf/compiler/plugin_pb";
 import descriptorPb from "google-protobuf/google/protobuf/descriptor";
-import { DescriptorProto } from "../node_modules/@types/google-protobuf/google/protobuf/descriptor_pb";
+import {
+  DescriptorProto,
+  FieldDescriptorProto
+} from "../node_modules/@types/google-protobuf/google/protobuf/descriptor_pb";
 
 function tou8(buf: any) {
   if (!buf) return undefined;
@@ -37,6 +40,45 @@ const CodeGeneratorResponseError = (stdout = process.stdout) => err => {
   stdout.write(Buffer.from(out.serializeBinary()));
 };
 
+function fieldToName(field: FieldDescriptorProto.AsObject): string {
+  switch (field.type) {
+    case 1: // DOUBLE
+    case 2: // FLOAD
+    case 6: // FIXED64
+    case 7: // FIXED32
+    case 15: // SFIXED32
+    case 16: // SFIXED64
+      return "Float";
+    case 3: // INT64
+    case 4: // UINT64
+    case 5: // INT32
+    case 13: // UINT32
+    case 17: // SINT32
+    case 18: // SINT64
+      return "Number";
+    case 8: // STRING
+    case 12: // BYTES
+      return "Boolean";
+    case 9:
+      return "String";
+    case 10:
+      throw new Error("Group is not supported");
+    case 11:
+      const splitted = field.typeName!.split(".");
+      return splitted[splitted.length - 1];
+    case 13: // ENUM
+      return "Enum";
+    default:
+      throw new Error(
+        `Not supported type: ${field}. see: https://github.com/google/protobuf/blob/master/src/google/protobuf/descriptor.proto`
+      );
+  }
+}
+
+function isRepeatedField(field: FieldDescriptorProto.AsObject) {
+  return field.label === 3;
+}
+
 CodeGeneratorRequest()
   .then(r => {
     const req = r.toObject();
@@ -46,9 +88,23 @@ CodeGeneratorRequest()
   })
   .then(protos =>
     protos.map(proto => {
+      const typeStrs: string[] = [];
+      proto.messageTypeList.forEach(message => {
+        let typeStr = "";
+        typeStr += `type ${message.name} {\n`;
+        message.fieldList.forEach(field => {
+          let fieldName = fieldToName(field);
+          if (isRepeatedField(field)) {
+            fieldName = `[${fieldName}]!`;
+          }
+          typeStr += `  ${field.name}: ${fieldName}\n`;
+        });
+        typeStr += "}";
+        typeStrs.push(typeStr);
+      });
       return {
-        name: `${(proto as any).pb_package}.hello`,
-        content: JSON.stringify(proto)
+        name: `${(proto as any).pb_package}.graphql`,
+        content: typeStrs.join("\n\n")
       };
     })
   )
