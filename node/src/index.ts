e81dfe0d2824e645190773096761e33307a81360
KentoMoriwaki <kento@wantedly.com>
2018-07-29 20:36:26 +0900

    Make fields

--- node/src/index.ts
+++ node/src/index.ts
@@ -3,6 +3,7 @@ import fs from "fs";
 import path from "path";
 import grpc from "grpc";
 import { FieldMask } from "google-protobuf/google/protobuf/field_mask_pb";
+import { FieldNode, visit } from "graphql/language";
 
 import messages from "../generated/posts_pb";
 import services from "../generated/posts_grpc_pb";
@@ -26,28 +27,43 @@ const typeDefs = gql`
   }
 `;
 
+function makeFieldMask(fields: Array<FieldNode>): FieldMask {
+  const fm = new FieldMask();
+  const pathsList: Array<string> = [];
+  fields.forEach(field => {
+    const stacked: string[] = [];
+    visit(field, {
+      enter: {
+        Field(node) {
+          stacked.push(node.name.value);
+        }
+      },
+      leave: {
+        Field(node) {
+          const path = stacked.slice(1).join(".");
+          if (path) {
+            pathsList.push(path);
+          }
+          stacked.splice(-1, 1);
+        }
+      }
+    });
+  });
+  fm.setPathsList(pathsList);
+  return fm;
+}
+
 // A map of functions which return data for the schema.
 const resolvers = {
   Query: {
     hello: (root, args, context, info) => {
-      const fields = new FieldMask();
-      fields.setPathsList([
-        "id",
-        "title",
-        "user.id",
-        "user.name",
-        "user.profile.introduction",
-        "user.posts.id",
-        "user.posts.description"
-      ]);
       const req = new messages.PostsListRequest();
-      req.setFields(fields);
+      req.setFields(makeFieldMask(info.fieldNodes));
       return new Promise((resolve, reject) => {
         client.list(req, (err, res) => {
           if (err) {
             reject(err);
           } else {
-            // console.log(root, args, context, info);
             resolve(res.toObject().postsList);
           }
         });
